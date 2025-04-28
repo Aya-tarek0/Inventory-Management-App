239b0ccb5e55d8f8229ec51b249b70c29f940f7f
Aya-tarek0 <ayatttarek@gmail.com>
2025-04-28 19:40:33 +0300

    Initial commit

---
+++ Inventory Management System/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Application;
+using Infrastructure;
+namespace Inventory_Management_System
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                   .AddInfDI();
+            return services;
+
+        }
+    }
+}
---
+++ MyApp.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+        {
+
+            return services;
+
+        }
+            
+    }
+}
